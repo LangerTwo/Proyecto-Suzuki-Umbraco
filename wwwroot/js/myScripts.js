0d470a79aa737cac6d9d8943a7333ea68cfb784e
LangerTwo <adrangel95@gmail.com>
2023-11-24 16:59:28 -0500

    modelo casi terminado falta la pestaña Todos y los estilos

---
+++ wwwroot/js/myScripts.js
@@ -0,0 +1,17 @@
+const btnClick = document.querySelectorAll('#subModelo .btn-link');
+const showModeloList = document.querySelectorAll('#infoModelo');
+
+btnClick.forEach((btn, index) => {
+    btn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const showModelo = showModeloList[index];
+        showModelo.classList.add('showModelo');
+
+        showModeloList.forEach( (element, idx) => {
+            if (idx !== index) {
+                element.classList.remove('showModelo');
+            }
+        });
+    });
+});
