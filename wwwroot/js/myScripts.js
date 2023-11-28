bd24c420daaf6b2cfc4b41a0338ba15fd8743c4a
LangerTwo <adrangel95@gmail.com>
2023-11-28 14:30:58 -0500

    se agrego el evento mouse a modelos

--- wwwroot/js/myScripts.js
+++ wwwroot/js/myScripts.js
@@ -1,15 +1,38 @@
 const btnClick = document.querySelectorAll('#subModelo .btn-link');
 const showModeloList = document.querySelectorAll('#infoModelo');
+const modelos = document.querySelector('nav #modelos');
+const subModelo = document.querySelector('#subModelo');
+console.log(subModelo)
 
 btnClick.forEach((btn, index) => {
     btn.addEventListener('click', (e) => {
         e.preventDefault();
+
         const showModelo = showModeloList[index];
         showModelo.classList.add('showModelo');
+        // console.log(e.target)
         showModeloList.forEach( (element, idx) => {
             if (idx !== index) {
+                e.preventDefault();
                 element.classList.remove('showModelo');
             }
         });
+
+        const btnStyle = btnClick[index]
+        btnStyle.classList.add('active')
+        btnClick.forEach( (element, idx) => {
+            if(idx !== index){
+                e.preventDefault();
+                element.classList.remove('active');
+            }
+        })
     });
+});
+
+modelos.addEventListener('mouseenter', () => {
+    subModelo.classList.add('show');
+});
+
+subModelo.addEventListener('mouseleave', (e) => {
+    subModelo.classList.remove('show');       
 });
