571b94dfb30c50d178bb6ac60256a512442dd2ed
LangerTwo <adrangel95@gmail.com>
2023-11-28 17:00:52 -0500

    agregando arrow a navbar

--- wwwroot/js/myScripts.js
+++ wwwroot/js/myScripts.js
@@ -4,51 +4,101 @@ const showLista = document.querySelectorAll('nav #lista .dropdown');
 const subMenu = document.querySelectorAll('.dropdown-menu');
 console.log(subMenu)
 
-btnClick.forEach((btn, index) => {
-    btn.addEventListener('click', (e) => {
-        e.preventDefault();
+document.addEventListener('DOMContentLoaded', () => {
+    showModel();
+    listaShow();
+    closeShow();
+ });
 
-        const showModelo = showModeloList[index];
-        showModelo.classList.add('showModelo');
-        // console.log(e.target)
-        showModeloList.forEach( (element, idx) => {
-            if (idx !== index) {
-                e.preventDefault();
-                element.classList.remove('showModelo');
-            }
+ 
+ // Funciones
+function showModel() {
+     btnClick.forEach((btn, index) => {
+         btn.addEventListener('click', (e) => {
+             e.preventDefault();
+     
+             const showModelo = showModeloList[index];
+             showModelo.classList.add('showModelo');
+             // console.log(e.target)
+             showModeloList.forEach( (element, idx) => {
+                 if (idx !== index) {
+                     e.preventDefault();
+                     element.classList.remove('showModelo');
+                 }
+             });
+     
+             const btnStyle = btnClick[index]
+             btnStyle.classList.add('active')
+             btnClick.forEach( (element, idx) => {
+                 if(idx !== index){
+                     e.preventDefault();
+                     element.classList.remove('active');
+                 }
+             })
+         });
+     });
+}
+
+function listaShow() {
+    showLista.forEach((btn, index) => {
+        btn.addEventListener('mouseenter', (e) => {
+            e.preventDefault();
+
+            const listaShow = subMenu[index]
+            listaShow.classList.add('show');
+            subMenu.forEach( (element, idx) => {
+                if (idx !== index) {
+                    e.preventDefault();
+                    element.classList.remove('show');  
+                }
+            })
         });
+    })
+    
+    showLista.forEach((btn, index) => {
+        btn.addEventListener('click', (e) => {
+            e.preventDefault();
 
-        const btnStyle = btnClick[index]
-        btnStyle.classList.add('active')
-        btnClick.forEach( (element, idx) => {
-            if(idx !== index){
-                e.preventDefault();
-                element.classList.remove('active');
-            }
-        })
-    });
-});
+            const listaShow = subMenu[index]
+            listaShow.classList.add('show');
+            subMenu.forEach( (element, idx) => {
+                if (idx !== index || idx === index) {
+                    e.preventDefault();
+                    element.classList.remove('show');  
+                }
+            })
+        });
+    })
 
-showLista.forEach((btn, index) => {
-    btn.addEventListener('mouseenter', () => {
-        const listaShow = subMenu[index]
-        listaShow.classList.add('show');
-        subMenu.forEach( (element, idx) => {
-            if (idx !== index) {
-                element.classList.remove('show');  
-            }
-        })
-    });
-})
+    showLista.forEach((btn, index) => {
+        btn.addEventListener('touchstart', (e) => {
+            e.preventDefault();
 
-subMenu.forEach((btn, index) => {
-    btn.addEventListener('mouseleave', () => {
-        const listaShow = subMenu[index]
-        listaShow.classList.remove('show');
-        subMenu.forEach( (element, idx) => {
-            if (idx !== index) {
-                element.classList.remove('show');  
-            }
-        })
-    });
-})
+            const listaShow = subMenu[index]
+            listaShow.classList.add('show');
+            subMenu.forEach( (element, idx) => {
+                if (idx !== index || idx === index) {
+                    e.preventDefault();
+                    element.classList.remove('show');  
+                }
+            })
+        });
+    })
+}
+
+function closeShow() {
+    subMenu.forEach((btn, index) => {
+        btn.addEventListener('mouseleave', (e) => {
+            e.preventDefault();
+
+            const listaShow = subMenu[index]
+            listaShow.classList.remove('show');
+            subMenu.forEach( (element, idx) => {
+                if (idx !== index) {
+                    e.preventDefault();
+                    element.classList.remove('show');  
+                }
+            })
+        });
+    })
+}
